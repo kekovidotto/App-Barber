6efce6e3921e913eade6b35da84e32c14cbcbf66
QUESLON VIDOTTO DE OLIVEIRA <75051018+kekovidotto@users.noreply.github.com>
2024-12-12 11:32:37 -0300

    feat: add phone item component and Footer to layout

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
@@ -28,6 +29,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   if (!barbershop) {
     return notFound()
   }
+
   return (
     <div>
       {/*IMAGE*/}
@@ -53,12 +55,12 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           size="icon"
           variant="secondary"
           className="absolute right-4 top-4"
-          asChild
         >
           <MenuIcon />
         </Button>
       </div>
 
+      {/*TITULO*/}
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
         <div className="item-center mb-2 flex gap-2">
@@ -71,13 +73,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
       </div>
+
       {/*DESCRIÇÃO*/}
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
       </div>
+
       {/*SERVIÇOS*/}
-      <div className="space-y-3 p-5">
+      <div className="space-y-3 border-b border-solid p-5">
         <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
           Serviços
         </h2>
@@ -86,6 +90,13 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
             <ServiceItem key={service.id} service={service} />
           ))}
         </div>
+      </div>
+
+      {/*CONTATO*/}
+      <div className="space-y-3 p-5">
+        {barbershop.phones.map((phone) => (
+          <PhoneItem key={phone} phone={phone} />
+        ))}
       </div>
     </div>
   )
